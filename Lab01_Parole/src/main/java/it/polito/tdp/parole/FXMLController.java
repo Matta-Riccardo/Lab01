d91aedaf98163d6bbe4d6c1b27b635bd45636b3e
riccardo <riccardo@LAPTOP-28JRDE4L>
2022-09-17 01:31:04 +0200

    prima soluzione del laboratorio

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -7,12 +7,13 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
 	
-	Parole elenco ;
+	Parole elenco;
 
     @FXML
     private ResourceBundle resources;
@@ -31,15 +32,75 @@ public class FXMLController {
 
     @FXML
     private Button btnReset;
+    
+    @FXML
+    private Button btnDelete;
+    
+    @FXML
+    private Label txtTime; 
 
     @FXML
     void doInsert(ActionEvent event) {
-    	// TODO
+    	txtTime.setText("");
+    	long start, end;
+    	start = System.currentTimeMillis();
+    	
+    	elenco.addParola(txtParola.getText());
+    	String tutte = "";
+    	for(String s : elenco.getElenco()) {
+    		if(tutte!=null) {
+    			tutte += "\n";
+    		}
+    		tutte += s;
+    	}
+    	
+    	txtResult.setText(tutte);
+    	txtParola.setText("");
+    	
+    	end = System.currentTimeMillis();
+    	txtTime.setText("tempo impiegato = " + (end-start)+ " ms");
+    }
+    
+    @FXML
+    void doDeleteWord(ActionEvent event) {
+    	txtTime.setText("");
+    	long start, end;
+    	start = System.currentTimeMillis();
+    	
+    	String word = txtResult.getSelectedText();
+    	elenco.removeParola(word);
+    	
+    	String tutte = "";
+    	for(String s : elenco.getElenco()) {
+    		if(tutte!=null) {
+    			tutte += "\n";
+    		}
+    		tutte += s;
+    	}
+    	
+    	txtResult.setText(tutte);
+    	end = System.currentTimeMillis();
+    	txtTime.setText("tempo impiegato = " + (end-start)+ " ms");
+    	
     }
 
     @FXML
     void doReset(ActionEvent event) {
-    	// TODO
+    	txtTime.setText("");
+    	long start, end;
+    	
+    	start = System.currentTimeMillis();
+    	elenco.reset();
+    	String tutto = "";
+    	
+    	for(String i : elenco.getElenco()) {
+    	tutto += i; 
+    	}
+    	
+    	txtResult.setText(tutto);
+    	txtParola.setText("");
+    	end = System.currentTimeMillis();
+    	txtTime.setText("tempo impiegato = " + (end-start)+ " ms");
     }
 
     @FXML
@@ -48,6 +109,8 @@ public class FXMLController {
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnDelete != null : "fx:id=\"btnDelete\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
 
         elenco = new Parole() ;
     }
